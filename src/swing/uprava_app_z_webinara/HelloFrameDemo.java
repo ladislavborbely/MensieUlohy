d7e006cc31102e53639ee977967f0e5bbf71e6a4
ladislavborbely <ladislavborbely@gmail.com>
2017-09-19 19:58:18 +0200

    thread safe

--- src/swing/uprava_app_z_webinara/HelloFrameDemo.java
+++ src/swing/uprava_app_z_webinara/HelloFrameDemo.java
@@ -1,13 +1,18 @@
 package swing.uprava_app_z_webinara;
 
+import java.awt.EventQueue;
+
 import javax.swing.JFrame;
 import javax.swing.WindowConstants;
 
 public class HelloFrameDemo {
 	public static void main(String[] args) {
-		JFrame frame = new HelloFrame();
-		frame.setTitle("Hello world in Swing");
-		frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-		frame.setVisible(true);
+
+		EventQueue.invokeLater(() -> {
+			JFrame frame = new HelloFrame();
+			frame.setTitle("Hello world in Swing");
+			frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+			frame.setVisible(true);
+		});
 	}
 }
